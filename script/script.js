bf77b324db0ef5217a73de542f7967c34facdd2a
jssroberto <rovertyto@outlook.com>
2024-10-20 23:25:42 -0700

    Aside casi listo
    Faltan unos detallitos

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 const page1 = document.querySelector(".register-page1");
 const page2 = document.querySelector(".register-page2");
-const leftRadio = document.querySelector("#leftRadio")
-const rightRadio = document.querySelector("#rightRadio")
+const leftRadio = document.querySelector("#leftRadio");
+const rightRadio = document.querySelector("#rightRadio");
 const arrowRight = document.querySelector(".arrow-register-right");
 const arrowLeft = document.querySelector(".arrow-register-left");
 const formRegister = document.querySelector(".form-register");
@@ -23,52 +23,56 @@ arrowLeft.addEventListener("click", () => cambiarPantalla(true));
 // });
 
 avatar.addEventListener("change", (e) => {
-	if (e.target.files[0]) {
-		const reader = new FileReader();
-		reader.onload = (e) => {
-			img.src = e.target.result;
-		}
-		reader.readAsDataURL(e.target.files[0])
-	} else {
-		img.src = defaultFile;
-	}
+  if (e.target.files[0]) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      img.src = e.target.result;
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  } else {
+    img.src = defaultFile;
+  }
 });
 
 // Se podrían llegar a utilizar promises/async/await si queremos utilizar transiciones más complejas
 function cambiarPantalla(isPageOneVisible) {
-	if (isPageOneVisible) {
-		arrowLeft.style.opacity = "0";
-		arrowRight.style.opacity = "1";
-		page2.style.display = "none";
-		page1.style.display = "block";
-		leftRadio.checked = true;
-		page2.style.left = "100px";
-		page2.style.opacity = "0";
-		containerBtn.style.display = "none";
-		containerBtn.style.left = "100px";
-		titleRegister.style.display = "block";
-		setTimeout(() => {
-			titleRegister.style.right = "0";
-			page1.style.right = "0";
-			page1.style.opacity = "1";
-		}, 10)
-	} else {
-		arrowLeft.style.opacity = "1";
-		arrowRight.style.opacity = "0";
-		page1.style.display = "none";
-		page2.style.display = "block";
-		rightRadio.checked = true;
-		page1.style.right = "100px";
-		page1.style.opacity = "0";
-		containerBtn.style.display = "block";
-		containerBtn.style.right = "100px";
-		titleRegister.style.display = "none";
-		titleRegister.style.right = "100px";
-		setTimeout(() => {
-			containerBtn.style.left = "0";
-			page2.style.left = "0";
-			page2.style.opacity = "1";
-		}, 10)
-	}
+  if (isPageOneVisible) {
+    arrowLeft.style.opacity = "0";
+    arrowRight.style.opacity = "1";
+    page2.style.display = "none";
+    page1.style.display = "block";
+    leftRadio.checked = true;
+    page2.style.left = "100px";
+    page2.style.opacity = "0";
+    containerBtn.style.display = "none";
+    containerBtn.style.left = "100px";
+    titleRegister.style.display = "block";
+    setTimeout(() => {
+      titleRegister.style.right = "0";
+      page1.style.right = "0";
+      page1.style.opacity = "1";
+    }, 10);
+  } else {
+    arrowLeft.style.opacity = "1";
+    arrowRight.style.opacity = "0";
+    page1.style.display = "none";
+    page2.style.display = "block";
+    rightRadio.checked = true;
+    page1.style.right = "100px";
+    page1.style.opacity = "0";
+    containerBtn.style.display = "block";
+    containerBtn.style.right = "100px";
+    titleRegister.style.display = "none";
+    titleRegister.style.right = "100px";
+    setTimeout(() => {
+      containerBtn.style.left = "0";
+      page2.style.left = "0";
+      page2.style.opacity = "1";
+    }, 10);
+  }
 }
 
+function redirect() {
+  var select = document.getElementById("mythologySelect");
+  window.location.href = select.value;
+}
